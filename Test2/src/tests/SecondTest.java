93aa60413d415e30b32ad60a42b313d5162b39cc
kmittal12 <mittal.kanika12@gmail.com>
2017-03-19 15:44:12 +0530

    conflict example

--- Test2/src/tests/SecondTest.java
+++ Test2/src/tests/SecondTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 public class SecondTest {
 	TransformerFactory fact = new net.sf.saxon.TransformerFactoryImpl();
 	@Test
+//conflict-1
 	public void testSecond()
 	{
 		System.out.println("hey");
